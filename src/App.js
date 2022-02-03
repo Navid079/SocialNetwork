01e57551c8a2078b76929c0c8cec1683b6793872
Navid <navid.naseri.079@gmail.com>
2022-02-03 03:51:38 +0330

    new post component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import NewPost from './components/NewPost/NewPost';
 
 function App() {
-  return <div className='App' />;
+  return (
+    <div className='App'>
+      <NewPost />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NewPost/NewPost.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from '../UI/Button';
+
+import './NewPost.css';
+
+const NewPost = ({ onPost }) => {
+  return (
+    <div className='new-post'>
+      <div className='new-post__body'>
+        <div className='new-post__avatar-container'>
+          <img
+            className='new-post__avatar'
+            src={require('../../assets/pp4.jpg')}
+          />
+        </div>
+        <textarea
+          className='new-post__input'
+          placeholder='Write Something...'
+        />
+      </div>
+      <div className='new-post__splitter' />
+      <div className='new-post__controls'>
+        <div className='new-post__media'></div>
+        <Button>Post</Button>
+      </div>
+    </div>
+  );
+};
+
+export default NewPost;
